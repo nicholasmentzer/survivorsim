d7aad662a48dd93fb02d5aed6108faf15b4dbdc5
BEHEEYEMPC\0816n <ntm224@lehigh.edu>
2025-02-28 11:45:39 -0500

    fixed vote details layout

--- src/app/page.js
+++ src/app/page.js
@@ -497,7 +497,7 @@ export default function Home() {
                                 ))}
                               </div>
                             : event.type === "voting" ?
-                              <div className="bg-stone-800 text-white px-6 py-3 rounded-lg shadow-md text-center w-1/2">
+                              <div className="bg-stone-800 text-white px-6 py-3 rounded-lg shadow-md text-center sm:w-3/4 md:w-1/2">
                                 {event.message.map((vote, i) => {
                                   if(!vote.includes("voted for")){ return(<div key={i} className="text-xs sm:text-sm mb-1" dangerouslySetInnerHTML={{ __html: vote }}></div>); } else { 
                                     let action = "voted for";
